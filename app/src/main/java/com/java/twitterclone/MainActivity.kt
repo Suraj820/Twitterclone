2630a9a20325437926a6fe908a3df98ee00d4751
suraj <surajpal.enjay@gmail.com>
2022-10-01 10:26:38 +0530

    Added Firebase Dependency

--- app/src/main/java/com/java/twitterclone/MainActivity.kt
+++ app/src/main/java/com/java/twitterclone/MainActivity.kt
@@ -2,10 +2,20 @@ package com.java.twitterclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        var currentUser = auth.getCurrentUser()
     }
 }
